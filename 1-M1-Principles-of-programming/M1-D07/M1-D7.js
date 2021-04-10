d224b63471052da8c3b86f3681aaa384672d75b9
MSajedian <mohammadsajedian@gmail.com>
2021-04-10 14:30:10 +0100

    delete style tag and create it by M1-D7.js

--- 1-M1-Principles-of-programming/M1-D07/M1-D7.js
+++ 1-M1-Principles-of-programming/M1-D07/M1-D7.js
@@ -2,134 +2,139 @@
       Write a function for changing the title of the document in something else.
       */
 
-      const changeTitle = function (newTitle) {
-        let title = document.querySelector("header h1");
-        title.innerHTML = "title changed by JS codes";
-      };
+const changeTitle = function (newTitle) {
+  let title = document.querySelector("header h1");
+  title.innerHTML = "title changed by JS codes";
+};
 
-      /* EXERCISE 2
-      Write a function for changing the class of the title of the page in "myHeading".
-      */
+/* EXERCISE 2
+Write a function for changing the class of the title of the page in "myHeading".
+*/
 
-      const addClassToTitle = function () {
-        let title = document.querySelector("header h1");
-        title.classList.add("myHeading");
-      };
+const addClassToTitle = function () {
+  let title = document.querySelector("header h1");
+  title.classList.add("myHeading");
+};
 
-      /* EXERCISE 3
-      Write a function for changing the text of only the p which are children of a div.
-      */
 
-      const changePcontent = function () {
-        let pChildrenOfDiv = document.querySelectorAll("div p");
-        for (i = 0; i < pChildrenOfDiv.length; i++) {
-          pChildrenOfDiv[i].innerHTML = "new text";
-        }
-      };
 
-      /* EXERCISE 4
-      Write a function for changing the destination of every link to https://www.google.com
-      */
+/* EXERCISE 3
+Write a function for changing the text of only the p which are children of a div.
+*/
 
-      const changeUrls = function () {
-        let links = document.querySelectorAll("a");
-        for (i = 0; i < links.length; i++) {
-          links[i].setAttribute("href", "https://www.google.com");
-        }
-      };
+const changePcontent = function () {
+  let pChildrenOfDiv = document.querySelectorAll("div p");
+  for (i = 0; i < pChildrenOfDiv.length; i++) {
+    pChildrenOfDiv[i].innerHTML = "new text";
+  }
+};
 
-      /* EXERCISE 5
-       Write a function for adding a new item in the second list.
-       */
+/* EXERCISE 4
+Write a function for changing the destination of every link to https://www.google.com
+*/
 
-      const addToTheSecondList = function (content) {
-        let secondList = document.querySelector("ul#secondList");
-        let newListItem = document.createElement("li");
-        newListItem.innerText = "4th";
-        newListItem.style.color = "red";
-        secondList.appendChild(newListItem);
-      };
+const changeUrls = function () {
+  let links = document.querySelectorAll("a");
+  for (i = 0; i < links.length; i++) {
+    links[i].setAttribute("href", "https://www.google.com");
+  }
+};
 
-      /* EXERCISE 6
-      Write a function for adding a second paragraph to the div.
-      */
+/* EXERCISE 5
+ Write a function for adding a new item in the second list.
+ */
 
-      const addParagraph = function (content) {
-        let firstDivTag = document.querySelector("div");
-        let newParagraph = document.createElement("p");
-        newParagraph.innerText = "This is a new Paragraph";
-        newParagraph.style.color = "red";
-        firstDivTag.appendChild(newParagraph);
-      };
+const addToTheSecondList = function (content) {
+  let secondList = document.querySelector("ul#secondList");
+  let newListItem = document.createElement("li");
+  newListItem.innerText = "4th";
+  newListItem.style.color = "red";
+  secondList.appendChild(newListItem);
+};
 
-      /* EXERCISE 7
-      Write a function for making the first UL disappear.
-      */
+/* EXERCISE 6
+Write a function for adding a second paragraph to the div.
+*/
 
-      const firstUlDisappear = function () {
-        let FirstUL = document.querySelectorAll("ul")[0];
-        console.log(FirstUL);
-        FirstUL.style.display = "none";
-      };
+const addParagraph = function (content) {
+  let firstDivTag = document.querySelector("div");
+  let newParagraph = document.createElement("p");
+  newParagraph.innerText = "This is a new Paragraph";
+  newParagraph.style.color = "red";
+  firstDivTag.appendChild(newParagraph);
+};
 
-      /* EXERCISE 8
-      Write a function for making the background of every UL green.
-      */
+/* EXERCISE 7
+Write a function for making the first UL disappear.
+*/
 
-      const paintItGreen = function () {
-        let everyUL = document.querySelectorAll("ul");
-        for (i = 0; i < everyUL.length; i++) {
-          everyUL[i].style.background = "green";
-        }
-      };
+const firstUlDisappear = function () {
+  let FirstUL = document.querySelectorAll("ul")[0];
+  console.log(FirstUL);
+  FirstUL.style.display = "none";
+};
 
-      /* EXERCISE 9
-      Make the heading of the page change color every time the user clicks on it.
-      */
+/* EXERCISE 8
+Write a function for making the background of every UL green.
+*/
 
-      const makeItClickable = function () {
-        let headingOfThePage = document.querySelector("h1");
-        headingOfThePage.onclick = changeColor;
-      };
+const paintItGreen = function () {
+  let everyUL = document.querySelectorAll("ul");
+  for (i = 0; i < everyUL.length; i++) {
+    everyUL[i].style.background = "green";
+  }
+};
 
-      const changeColor = function (e) {
-        console.log(e.target);
-        e.target.style.color = "red";
-      };
+/* EXERCISE 9
+Make the heading of the page change color every time the user clicks on it.
+*/
 
-      /* EXERCISE 10
-      Change the footer text with something else when the user clicks on it.
-      */
+const makeItClickable = function () {
+  let headingOfThePage = document.querySelector("h1");
+  headingOfThePage.onclick = changeColor;
+};
 
-      const changeFooterText = function () {
-        let footer = document.querySelector("footer");
-        footer.onclick = changeText;
-      };
+const changeColor = function (e) {
+  console.log(e.target);
+  e.target.style.color = "red";
+};
 
-      const changeText = function (e) {
-        console.log(e.target);
-        e.target.innerText = "new text";
-      };
-      /* EXERCISE 11
-      Attach an event listener to the input field in the page for console logging its value just after any keystroke.
-      */
+/* EXERCISE 10
+Change the footer text with something else when the user clicks on it.
+*/
 
-      const inputField = document.getElementById("input-field");
-      inputField.addEventListener("keydown", (e) => {
-        console.log(e.target.value);
-      });
+const changeFooterText = function () {
+  let footer = document.querySelector("footer");
+  footer.onclick = changeText;
+};
 
-      /* EXERCISE 12
-      Create a new welcome alert message when the page successfully loads.
-      */
+const changeText = function (e) {
+  console.log(e.target);
+  e.target.innerText = "new text";
+};
+/* EXERCISE 11
+Attach an event listener to the input field in the page for console logging its value just after any keystroke.
+*/
 
-      window.onload = function () {
-        alert("Welcome to this page");
-      };
+const inputField = document.getElementById("input-field");
+inputField.addEventListener("keydown", (e) => {
+  console.log(e.target.value);
+});
 
-      /* EXERCISE 13
-      Use HTML5 tags to divide the content of the page in a more semantic way.
-      */
+/* EXERCISE 12
+Create a new welcome alert message when the page successfully loads.
+*/
+
+window.onload = function () {
+  alert("Welcome to this page");
+  let styleElement = document.createElement("style");
+  styleElement.innerText = ".myHeading {color: red;}"
+  let headElement = document.querySelector("head");
+  headElement.appendChild(styleElement)
+};
+
+/* EXERCISE 13
+Use HTML5 tags to divide the content of the page in a more semantic way.
+*/
 
       // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML_sections_and_outlines
-    
