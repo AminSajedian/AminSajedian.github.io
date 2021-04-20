5326a3c3ba2bcfec962c353a878dc5f985bd9e25
MSajedian <mohammadsajedian@gmail.com>
2021-04-20 17:15:36 +0100

    M3-D7 updated

--- 4-M3-Advanced-JavaScript/M3-D07/index.js
+++ 4-M3-Advanced-JavaScript/M3-D07/index.js
@@ -19,29 +19,59 @@ window.onload = () => {
     }
 
     fetchUsersJSON().then((usersArray) => {
-        const Users = document.getElementById("Users");
-        usersArray.forEach((usersData) => {
-            Users.innerHTML += `<option value="Name: ${usersData.name} | Username: ${usersData.username} | Email: ${usersData.email}">Name: ${usersData.name} | Username: ${usersData.username} | Email: ${usersData.email}</option>`;
-        });
-    });
-}
-
-
-const searchUsersFun = () => {
-    const searchUsers = document.getElementById("searchUsers").value;
-    const Users = document.getElementById("Users");
-    Users.innerHTML = ""
-
-    const searchedUsersArray = MainUsersArray.filter(Users => Users.name.toLowerCase().indexOf(searchUsers) !== -1);
-    searchedUsersArray.forEach((usersData) => {
-        Users.innerHTML += `<option value="Name: ${usersData.name} | Username: ${usersData.username} | Email: ${usersData.email}">Name: ${usersData.name} | Username: ${usersData.username} | Email: ${usersData.email}</option>`;
+        const resultsDiv = document.querySelector("#results-div > table > tbody")
+        MainUsersArray.forEach((usersData) => {
+            resultsDiv.innerHTML += `
+            <tr>
+                <th scope="row">${usersData.id}</th>
+                <td><a href="details.html?id=10">${usersData.name}</a></td>
+               <td>${usersData.username}</td>
+               <td>${usersData.email}</td>
+               <td>${usersData.phone}</td>
+             </tr>
+            `;
+        })
     });
 }
 
 //       Ex2) Create a dropdown (<select>) that allows the user to select between name, username and email. 
 //            Create then a filter. When the user types in something, you should filter the user based on the input and on the value of the select.
 //            Es.: select on NAME. Filter input = Glenna, only user id number 9 should remain
+
+const searchInputFun = () => {
+    const searchInput = document.getElementById("searchInput").value;
+    const resultsDiv = document.querySelector("#results-div > table > tbody")
+    const searchBy = document.getElementById("searchBy").value;
+    console.log('searchBy:', searchBy)
+    resultsDiv.innerHTML = ""
+    let searchedUsersArray = ""
+    if (searchBy == "name") {
+        searchedUsersArray = MainUsersArray.filter(Users => Users.name.toLowerCase().indexOf(searchInput) !== -1);
+    } else if (searchBy == "username") {
+        searchedUsersArray = MainUsersArray.filter(Users => Users.username.toLowerCase().indexOf(searchInput) !== -1);
+    } else if (searchBy == "email") {
+        searchedUsersArray = MainUsersArray.filter(Users => Users.email.toLowerCase().indexOf(searchInput) !== -1);
+    }
+
+    searchedUsersArray.forEach((usersData) => {
+        resultsDiv.innerHTML += `
+            <tr>
+                <th scope="row">${usersData.id}</th>
+                <td><a href="details.html?id=10">${usersData.name}</a></td>
+               <td>${usersData.username}</td>
+               <td>${usersData.email}</td>
+               <td>${usersData.phone}</td>
+             </tr>
+            `;
+    })
+}
+
+
+
+
 //       Ex3) Create a function that, from the list of users, extracts only the name
+
+
 //       Ex4) Create a function that, from the list of users, creates an array of addresses as string and not as an object. Like:
 //               {
 //               "street": "Victor Plains",
