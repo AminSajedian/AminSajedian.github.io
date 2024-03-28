8d9a4fc15cc39a5af40372a79fd2aa0dc226dd53
Mohammadamin Sajedian <mohammadsajedian@gmail.com>
2024-03-28 12:12:18 +0000

    updated the color method of chosenCell

--- 1-M1-Principles-of-programming/Projects/Random-Numbers-For-EUROMILLIONS-Lottery/Random-Numbers-For-EUROMILLIONS-Lottery.js
+++ 1-M1-Principles-of-programming/Projects/Random-Numbers-For-EUROMILLIONS-Lottery/Random-Numbers-For-EUROMILLIONS-Lottery.js
@@ -1,67 +1,55 @@
+
+function boardMaker(parentId, maxCellNumber, rowNumber, columnNumber, buttonText) {
+  // ******** Create a table and tbody ********
+  const parentElement = document.getElementById(parentId);
+  const table = document.createElement("table");
+  parentElement.appendChild(table);
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+  // ******** Create a table cells with id ********
+  let cellNumber = 1
+  for (let firstIndex = 1; firstIndex <= rowNumber; firstIndex++) {
+    let tableRow = document.createElement("tr");
+    tbody.appendChild(tableRow);
+    for (let secondIndex = 1; secondIndex <= columnNumber && cellNumber <= maxCellNumber; secondIndex++) {
+      let tableCell = document.createElement("td");
+      tableCell.innerText = cellNumber
+      // ********** Add id to the table cell **********
+      tableCell.id = `${parentId}-cell-${cellNumber}`;
+      tableRow.appendChild(tableCell);
+      cellNumber++
+    }
+  }
+
+  // ******* Create a button for table *******
+  const button = document.createElement("button");
+  button.innerText = buttonText;
+  parentElement.appendChild(button);
+
+  // ******* Create a function for table button to make a random number *******
+  function randomNumberMaker() {
+    let randomNumber = Math.floor(Math.random() * maxCellNumber) + 1;
+    const chosenCell = document.getElementById(`${parentId}-cell-${randomNumber}`);
+
+    if (chosenCell.style.backgroundColor == "") {
+      chosenCell.style = "background-color: rgba(255, 255, 0, 0.2);"
+    } else if (chosenCell.style.backgroundColor == "rgba(255, 255, 0, 0.2)") {
+      chosenCell.style.backgroundColor = "rgba(255, 255, 0, 0.4)"
+    } else if (chosenCell.style.backgroundColor == "rgba(255, 255, 0, 0.4)") {
+      chosenCell.style.backgroundColor = "rgba(255, 255, 0, 0.6)"
+    } else if (chosenCell.style.backgroundColor == "rgba(255, 255, 0, 0.6)") {
+      chosenCell.style.backgroundColor = "rgba(255, 255, 0, 0.8)"
+    } else if (chosenCell.style.backgroundColor == "rgba(255, 255, 0, 0.8)") {
+      chosenCell.style.backgroundColor = "rgba(255, 255, 0, 1)"
+    }
+
+  }
+
+  button.onclick = randomNumberMaker
+}
+
 window.onload = () => {
-  // **************** MainBoard ****************
-  const mainBoard = document.getElementById("main-board");
-  const mainBoardTable = document.createElement("table");
-  mainBoard.appendChild(mainBoardTable);
-  const mainBoardTbody = document.createElement("tbody");
-  mainBoardTable.appendChild(mainBoardTbody);
-
-  // TODO change the name of cellNumberOfMainBoard to something better
-  let cellNumberOfMainBoard = 1
-  let maxCellNumberOfMainBoard = 50
-  for (let firstIndex = 1; firstIndex <= 5; firstIndex++) {
-    let mainBoardTableRow = document.createElement("tr");
-    mainBoardTbody.appendChild(mainBoardTableRow);
-    for (let secondIndex = 1; secondIndex <= 10 && cellNumberOfMainBoard <= maxCellNumberOfMainBoard; secondIndex++) {
-      let mainBoardTableCell = document.createElement("td");
-      mainBoardTableCell.innerText = ((firstIndex - 1) * 10 + secondIndex)
-      mainBoardTableCell.classList.add(`MainBoardCell${(firstIndex - 1) * 10 + secondIndex}`);
-      mainBoardTableRow.appendChild(mainBoardTableCell);
-      cellNumberOfMainBoard++
-    }
-  }
-
-  const buttonForMainBoard = document.createElement("button");
-  buttonForMainBoard.innerText = "Play MainBoard";
-  mainBoard.appendChild(buttonForMainBoard);
-
-  function randomNumberForMainBoard() {
-    let randomNumber = Math.floor(Math.random() * 50) + 1;
-    const mainBoardCell = document.getElementsByClassName(`MainBoardCell${randomNumber}`)[0];
-    mainBoardCell.classList.add("backgroundYellow")
-  }
-  buttonForMainBoard.onclick = randomNumberForMainBoard
-
-  // LuckyStar
-  const luckyStarBoard = document.getElementById("lucky-star-board");
-  const luckyStarBoardTable = document.createElement("table");
-  luckyStarBoard.appendChild(luckyStarBoardTable);
-  const luckyStarBoardTbody = document.createElement("tbody");
-  luckyStarBoardTable.appendChild(luckyStarBoardTbody);
-
-  let cellNumberOfLuckyStarBoard = 1
-  let maxCellNumberOfLuckyStarBoard = 12
-  for (let firstIndex = 1; firstIndex <= 2; firstIndex++) {
-    let luckyStarBoardTableRow = document.createElement("tr");
-    luckyStarBoardTbody.appendChild(luckyStarBoardTableRow);
-    for (let secondIndex = 1; secondIndex <= 10 && cellNumberOfLuckyStarBoard <= maxCellNumberOfLuckyStarBoard; secondIndex++) {
-      let luckyStarBoardTableCell = document.createElement("td");
-      luckyStarBoardTableCell.innerText = ((firstIndex - 1) * 10 + secondIndex)
-      luckyStarBoardTableCell.classList.add(`LuckyStarBoardCell${(firstIndex - 1) * 10 + secondIndex}`);
-      luckyStarBoardTableRow.appendChild(luckyStarBoardTableCell);
-      cellNumberOfLuckyStarBoard++
-    }
-  }
-
-  const buttonForLuckyStarBoard = document.createElement("button");
-  buttonForLuckyStarBoard.innerText = "Play Lucky Star Board";
-  luckyStarBoard.appendChild(buttonForLuckyStarBoard);
-
-  function randomNumberForLuckyStarBoard() {
-    let randomNumber = Math.floor(Math.random() * 12) + 1;
-    console.log('randomNumber:', randomNumber)
-    const luckyStarBoardCell = document.getElementsByClassName(`LuckyStarBoardCell${randomNumber}`)[0];
-    luckyStarBoardCell.classList.add("backgroundYellow")
-  }
-  buttonForLuckyStarBoard.onclick = randomNumberForLuckyStarBoard
+  boardMaker("main-board", 50, 5, 10, "Play Main Board")
+  boardMaker("lucky-star-board", 12, 2, 6, "Play Lucky Star Board")
 }
